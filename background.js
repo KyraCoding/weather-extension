0924fc7c6b1bf09903df2ba193551cbc52cd4879
MrMe <76573108+MrMeCoding@users.noreply.github.com>
2024-08-18 19:19:25 -0400

    Add files via upload

--- background.js
+++ background.js
@@ -2,8 +2,16 @@
 chrome.runtime.onInstalled.addListener(async () => {
   console.log("Installed!")
 });
+
+async function getWeather() {
+  const response = await fetch("https://api.open-meteo.com/v1/forecast?latitude=39.2673&longitude=-76.7983&hourly=temperature_2m,precipitation_probability")
+  const data = response.json()
+  console.log(data)
+}
+
 self.addEventListener('message', function (event) {
   if (event.data && event.data.type === 'refresh') {
     console.log("Manual refresh initiated!")
+    getWeather()
   }
 })
