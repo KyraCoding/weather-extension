d6f566e6843df4f2da1fd08e6982c98f16aae8b3
MrMe <76573108+MrMeCoding@users.noreply.github.com>
2024-08-19 13:15:40 -0400

    Add files via upload

--- background.js
+++ background.js
@@ -13,5 +13,61 @@ self.addEventListener('message', function (event) {
   if (event.data && event.data.type === 'refresh') {
     console.log("Manual refresh initiated!")
     getWeather()
+    const location = await getGeolocation()
+    console.log(location)
   }
 })
+chrome.offscreen.createDocument({
+  url: 'offscreen.html',
+  reasons: [chrome.offscreen.Reason.GEOLOCATION || chrome.offscreen.Reason.DOM_SCRAPING],
+  justification: 'geolocation access',
+});
+const OFFSCREEN_DOCUMENT_PATH = '/offscreen.html';
+let creating; // A global promise to avoid concurrency issues
+
+chrome.runtime.onMessage.addListener(handleMessages);
+
+async function getGeolocation() {
+  await setupOffscreenDocument(OFFSCREEN_DOCUMENT_PATH);
+  const geolocation = await chrome.runtime.sendMessage({
+    type: 'get-geolocation',
+    target: 'offscreen'
+  });
+  await closeOffscreenDocument();
+  return geolocation;
+}
+
+async function hasDocument() {
+  // Check all windows controlled by the service worker to see if one
+  // of them is the offscreen document with the given path
+  const offscreenUrl = chrome.runtime.getURL(OFFSCREEN_DOCUMENT_PATH);
+  const matchedClients = await clients.matchAll();
+
+  return matchedClients.some(c => c.url === offscreenUrl)
+}
+
+async function setupOffscreenDocument(path) {
+  //if we do not have a document, we are already setup and can skip
+  if (!(await hasDocument())) {
+    // create offscreen document
+    if (creating) {
+      await creating;
+    } else {
+      creating = chrome.offscreen.createDocument({
+        url: path,
+        reasons: [chrome.offscreen.Reason.GEOLOCATION || chrome.offscreen.Reason.DOM_SCRAPING],
+        justification: 'add justification for geolocation use here',
+      });
+
+      await creating;
+      creating = null;
+    }
+  }
+}
+
+async function closeOffscreenDocument() {
+  if (!(await hasDocument())) {
+    return;
+  }
+  await chrome.offscreen.closeDocument();
+}
--- client.js
+++ client.js
@@ -1,7 +1,7 @@
 function setBackground(inp) {
     var videos = ["sunny.mp4","thunder.mp4","rainy.mp4","cloudy.mp4"]
     var video = document.getElementById("backgroundVideo")
-    video.src = inp ?? videos[Math.floor(Math.random()*videos.length)]
+    video.src = `assets/backgrounds/${inp ?? videos[Math.floor(Math.random()*videos.length)]}`
     console.log("Setting: "+video.src)
 }
 setBackground()
