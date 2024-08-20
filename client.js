6f710f90237e79027bab3e62f14c505ca5888ec1
MrMe <76573108+MrMeCoding@users.noreply.github.com>
2024-08-20 02:34:09 -0400

    Add files via upload

--- client.js
+++ client.js
@@ -37,15 +37,19 @@ function codeToBackground(code) {
 }
 async function getWeather(coords) {
     // ${coords.latitude}&longitude=${coords.longitude}
+    const cityName = (await (await fetch(`https://api-bdc.net/data/reverse-geocode-client?latitude=${coords.latitude}&longitude=${coords.longitude}&localityLanguage=en`)).json()).locality
     const response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${coords.latitude}&longitude=${coords.longitude}&current=temperature_2m,weather_code&hourly=temperature_2m,precipitation_probability,weather_code&daily=weather_code,temperature_2m_max,temperature_2m_min&temperature_unit=fahrenheit&timezone=auto`)
     const data = await response.json()
     console.log(data)
+    console.log(cityName)
 
     var currentWeather = codeToBackground(data.current.weather_code)
     document.getElementById("currentWeather").innerHTML = currentWeather
     document.getElementById("currentTemp").innerHTML = Math.round(data.current.temperature_2m)
-    document.getElementById("currentHigh").innerHTML = `H:${Math.round(data.daily.temperature_2m_max[0])}`
-    document.getElementById("currentLow").innerHTML = `L:${Math.round(data.daily.temperature_2m_min[0])}`
+    document.getElementById("tempType").innerHTML = data.current_units.temperature_2m[0]
+    document.getElementById("currentHigh").innerHTML = `H:${Math.round(data.daily.temperature_2m_max[0])}${data.daily_units.temperature_2m_max[0]}`
+    document.getElementById("currentLow").innerHTML = `L:${Math.round(data.daily.temperature_2m_min[0])}${data.daily_units.temperature_2m_min[0]}`
+    document.getElementById("cityName").innerHTML = cityName
 }
 navigator.geolocation.getCurrentPosition(function (position) {
     const coords = position.coords
