0cb256498ff64ec8e6923ea9572eead5f62b2709
MrMe <76573108+MrMeCoding@users.noreply.github.com>
2024-08-22 20:45:04 -0400

    Add files via upload

--- background.js
+++ background.js
@@ -9,50 +9,50 @@ chrome.alarms.onAlarm.addListener(async (alarm) => {
   }
 });
 
-// Manage service worker geolocation for background fetching 
-let creating;
+let creatingDocument = null
+
+async function createOffscreen() {
+  const existingContexts = await chrome.runtime.getContexts({ contextTypes: ['OFFSCREEN_DOCUMENT'] })
+  console.log(chrome.runtime.getURL("/offscreen.html"))
+  console.log(existingContexts)
+  // Can't create more than one!
+  if (existingContexts.length > 0) {
+    console.log("Offscreen docment already exists!")
+    return;
+  } else {
+    // Already being created
+    if (creatingDocument) {
+      console.log("Offscreen docment already being created!")
+      await creatingDocument;
+      createOffscreen()
+    } else {
+      creatingDocument = chrome.offscreen.createDocument({
+        url: 'offscreen.html',
+        reasons: ['GEOLOCATION'],
+        justification: 'To get the user location',
+      });
+      await creatingDocument;
+      console.log(await chrome.runtime.getContexts({ contextTypes: ['OFFSCREEN_DOCUMENT'] }))
+      creatingDocument = null;
+      console.log("Offscreen docment added!")
+    }
+  }
+}
 async function getGeolocation() {
-  await setupOffscreenDocument("/offscreen.html");
+  await createOffscreen()
   const geolocation = await chrome.runtime.sendMessage({
     type: 'get-geolocation',
     target: 'offscreen'
   });
-  await closeOffscreenDocument();
-  return geolocation;
-}
-async function hasDocument() {
-  const offscreenUrl = chrome.runtime.getURL("/offscreen.html");
-  const matchedClients = await clients.matchAll();
-
-  return matchedClients.some(c => c.url === offscreenUrl)
-}
-async function setupOffscreenDocument(path) {
-  if (!(await hasDocument())) {
-    if (creating) {
-      await creating;
-    } else {
-      creating = chrome.offscreen.createDocument({
-        url: path,
-        reasons: [chrome.offscreen.Reason.GEOLOCATION || chrome.offscreen.Reason.DOM_SCRAPING],
-        justification: 'add justification for geolocation use here',
-      });
-
-      await creating;
-      creating = null;
-    }
+  const existingContexts = await chrome.runtime.getContexts({ contextTypes: ['OFFSCREEN_DOCUMENT'] })
+  if (existingContexts.length > 0) {
+    await chrome.offscreen.closeDocument();
   }
-}
-async function closeOffscreenDocument() {
-  if (!(await hasDocument())) {
-    return;
-  }
-  await chrome.offscreen.closeDocument();
+  return geolocation
 }
 
-// Cache background requests
 async function cacheRequest() {
-  const newCache = await caches.open('weatherRequests');
   const location = await getGeolocation()
-  newCache.add(`https://api.open-meteo.com/v1/forecast?latitude=${location.coords.latitude}&longitude=${location.coords.longitude}&current=temperature_2m,weather_code&hourly=temperature_2m,precipitation_probability,is_day,weather_code&daily=weather_code,temperature_2m_max,temperature_2m_min,sunrise,sunset&temperature_unit=fahrenheit&timezone=auto`)
+  console.log(location.coords)
 }
 cacheRequest()
